1ba67a0e39764838e204a0b7adb01416454f8580
Mike Jenkins <kinetic@icloud.com>
2019-09-24 20:27:57 -0400

    Initial commit.

---
+++ departments.js
@@ -0,0 +1,18 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+const url = 'http://rehelv-acrd.tpsgc-pwgsc.gc.ca/acrds/preface-eng.aspx';
+
+const info = []; // result of data scrape
+
+rp(url)
+    .then((html) => {
+        let array = []
+        $('#wb-main-in > ul:nth-child(352)', html).children().each((i, item) => {
+            array.push(item.children[0].data)
+        });
+        console.log(JSON.stringify(array))
+    })
+    .catch((err) => {
+        //handle error
+        console.log(err);
+    })
